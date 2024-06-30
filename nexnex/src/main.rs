4e59d7c2328140ac33f21fae21ffbd8246d2e8c8
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-06-30 18:09:19 +0200

    use arrow keys for navigation, too

--- nexnex/src/main.rs
+++ nexnex/src/main.rs
@@ -106,13 +106,13 @@ fn move_player_from_input(
     input: Res<ButtonInput<KeyCode>>,
     level_walls: Res<LevelWalls>,
 ) {
-    let movement_direction = if input.just_pressed(KeyCode::KeyW) {
+    let movement_direction = if input.just_pressed(KeyCode::KeyW) || input.just_pressed(KeyCode::ArrowUp) {
         GridCoords::new(0, 1)
-    } else if input.just_pressed(KeyCode::KeyA) {
+    } else if input.just_pressed(KeyCode::KeyA) || input.just_pressed(KeyCode::ArrowLeft) {
         GridCoords::new(-1, 0)
-    } else if input.just_pressed(KeyCode::KeyS) {
+    } else if input.just_pressed(KeyCode::KeyS) || input.just_pressed(KeyCode::ArrowDown) {
         GridCoords::new(0, -1)
-    } else if input.just_pressed(KeyCode::KeyD) {
+    } else if input.just_pressed(KeyCode::KeyD) || input.just_pressed(KeyCode::ArrowRight) {
         GridCoords::new(1, 0)
     } else {
         return;
