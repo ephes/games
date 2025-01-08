0093a15317d7c08bed7a44c93499af0022704e3c
Jochen Wersdörfer <jochen@wersdoerfer.de>
2025-01-08 21:23:55 +0100

    use platformer example for bevy 0.15.1

--- nexnex/src/main.rs
+++ nexnex/src/main.rs
@@ -1,176 +1,47 @@
+// This example shows off a more in-depth implementation of a game with `bevy_ecs_ldtk`.
+// Please run with `--release`.
+
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
-use std::collections::HashSet;
 
-#[derive(Default, Component)]
-struct Player;
+use bevy_rapier2d::prelude::*;
 
-#[derive(Default, Bundle, LdtkEntity)]
-struct PlayerBundle {
-    player: Player,
-    #[sprite_sheet_bundle]
-    sprite_bundle: SpriteSheetBundle,
-    #[grid_coords]
-    grid_coords: GridCoords,
-}
-
-#[derive(Default, Component)]
-struct Goal;
-
-#[derive(Default, Bundle, LdtkEntity)]
-struct GoalBundle {
-    goal: Goal,
-    #[sprite_sheet_bundle]
-    sprite_bundle: SpriteSheetBundle,
-    #[grid_coords]
-    grid_coords: GridCoords,
-}
-
-#[derive(Default, Component)]
-struct Wall;
-
-#[derive(Default, Bundle, LdtkIntCell)]
-struct WallBundle {
-    wall: Wall,
-}
-
-#[derive(Default, Resource)]
-struct LevelWalls {
-    wall_locations: HashSet<GridCoords>,
-    level_width: i32,
-    level_height: i32,
-}
-
-impl LevelWalls {
-    fn in_wall(&self, grid_coords: &GridCoords) -> bool {
-        grid_coords.x < 0
-            || grid_coords.y < 0
-            || grid_coords.x >= self.level_width
-            || grid_coords.y >= self.level_height
-            || self.wall_locations.contains(grid_coords)
-    }
-}
+mod camera;
+mod climbing;
+/// Bundles for auto-loading Rapier colliders as part of the level
+mod colliders;
+mod enemy;
+/// Handles initialization and switching levels
+mod game_flow;
+mod ground_detection;
+mod inventory;
+mod misc_objects;
+mod player;
+mod walls;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(LdtkPlugin)
-        .add_systems(Startup, setup)
-        .insert_resource(LevelSelection::index(0))
-        .init_resource::<LevelWalls>()
-        .register_ldtk_entity::<PlayerBundle>("Player")
-        .register_ldtk_entity::<GoalBundle>("Goal")
-        .register_ldtk_int_cell::<WallBundle>(1)
-        .add_systems(
-            Update, 
-            (
-                move_player_from_input, 
-                translate_grid_coords_entities,
-                cache_wall_locations,
-                check_goal
-            )
-        )
+        .add_plugins((
+            LdtkPlugin,
+            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
+        ))
+        .insert_resource(LevelSelection::Uid(0))
+        .insert_resource(LdtkSettings {
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
+                load_level_neighbors: true,
+            },
+            set_clear_color: SetClearColor::FromLevelBackground,
+            ..Default::default()
+        })
+        .add_plugins(game_flow::GameFlowPlugin)
+        .add_plugins(walls::WallPlugin)
+        .add_plugins(ground_detection::GroundDetectionPlugin)
+        .add_plugins(climbing::ClimbingPlugin)
+        .add_plugins(player::PlayerPlugin)
+        .add_plugins(enemy::EnemyPlugin)
+        .add_systems(Update, inventory::dbg_print_inventory)
+        .add_systems(Update, camera::camera_fit_inside_current_level)
+        .add_plugins(misc_objects::MiscObjectsPlugin)
         .run();
-    println!("Hello, world!")
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let mut camera = Camera2dBundle::default();
-    camera.projection.scale = 0.5;
-    camera.transform.translation.x += 1280.0 / 4.0;
-    camera.transform.translation.y += 720.0 / 4.0;
-    commands.spawn(camera);
-
-    commands.spawn(LdtkWorldBundle {
-        ldtk_handle: asset_server.load("nexnex.ldtk"),
-        ..Default::default()
-    });
-}
-
-
-const GRID_SIZE: i32 = 16;
-
-fn translate_grid_coords_entities(
-    mut grid_coords_entities: Query<(&mut Transform, &GridCoords), Changed<GridCoords>>,
-) {
-    for (mut transform, grid_coords) in grid_coords_entities.iter_mut() {
-        transform.translation =
-            bevy_ecs_ldtk::utils::grid_coords_to_translation(*grid_coords, IVec2::splat(GRID_SIZE))
-                .extend(transform.translation.z);
-    }
-}
-
-
-fn move_player_from_input(
-    mut players: Query<&mut GridCoords, With<Player>>,
-    input: Res<ButtonInput<KeyCode>>,
-    level_walls: Res<LevelWalls>,
-) {
-    let movement_direction = if input.just_pressed(KeyCode::KeyW) || input.just_pressed(KeyCode::ArrowUp) {
-        GridCoords::new(0, 1)
-    } else if input.just_pressed(KeyCode::KeyA) || input.just_pressed(KeyCode::ArrowLeft) {
-        GridCoords::new(-1, 0)
-    } else if input.just_pressed(KeyCode::KeyS) || input.just_pressed(KeyCode::ArrowDown) {
-        GridCoords::new(0, -1)
-    } else if input.just_pressed(KeyCode::KeyD) || input.just_pressed(KeyCode::ArrowRight) {
-        GridCoords::new(1, 0)
-    } else {
-        return;
-    };
-
-    for mut player_grid_coords in players.iter_mut() {
-        let destination = *player_grid_coords + movement_direction;
-        if !level_walls.in_wall(&destination) {
-            *player_grid_coords = destination;
-        }
-    }
-}
-
-fn cache_wall_locations(
-    mut level_walls: ResMut<LevelWalls>,
-    mut level_events: EventReader<LevelEvent>,
-    walls: Query<&GridCoords, With<Wall>>,
-    ldtk_project_entities: Query<&Handle<LdtkProject>>,
-    ldtk_project_assets: Res<Assets<LdtkProject>>,
-) {
-    for level_event in level_events.read() {
-        if let LevelEvent::Spawned(level_iid) = level_event {
-            let ldtk_project = ldtk_project_assets
-                .get(ldtk_project_entities.single())
-                .expect("LdtkProject should be loaded when level is spawned");
-            let level = ldtk_project
-                .get_raw_level_by_iid(level_iid.get())
-                .expect("spawned level should exist in project");
-
-            let wall_locations = walls.iter().copied().collect();
-
-            let new_level_walls = LevelWalls {
-                wall_locations,
-                level_width: level.px_wid / GRID_SIZE,
-                level_height: level.px_hei / GRID_SIZE,
-            };
-
-            *level_walls = new_level_walls;
-        }
-    }
-}
-
-fn check_goal(
-    level_selection: ResMut<LevelSelection>,
-    players: Query<&GridCoords, (With<Player>, Changed<GridCoords>)>,
-    goals: Query<&GridCoords, With<Goal>>,
-) {
-    if players
-        .iter()
-        .zip(goals.iter())
-        .any(|(player_grid_coords, goal_grid_coords)| player_grid_coords == goal_grid_coords)
-    {
-        println!("Level complete!");
-        let indices = match level_selection.into_inner() {
-            LevelSelection::Indices(indices) => indices,
-            _ => panic!("level selection should always be Indices in this game"),
-        };
-
-        indices.level += 1;
-    }
 }
